8b0a071459cf17baa732d5cf307ba8fdf6730d42
Bas Jansen <tarskin@gmail.com>
2015-08-19 10:47:11 +0200

    Update vispy.py

--- vispy.py
+++ vispy.py
@@ -14,9 +14,26 @@ def quadratic(x,a,b,c):
 
 def power_law(x,a,b,c):
     return a*x**b+c
+    
+def movingaverage (values, window):
+    weights = np.repeat(1.0, window)/window
+    sma = np.convolve(values, weights, 'valid')
+    return sma
 
 def scatterplot_fit(X,Y,**kwargs):
-    """ WRITE DOCUMENTATION.
+    """
+    Takes the X and Y lists and plots them as a 2D scatter plot
+    through matplotlib. Additionally, the least squares fit is
+    plotted throughout the datapoints.
+    
+    Keyword arguments:
+    X -- List of the X-coordinates
+    Y -- List of the Y-coordinates
+    function -- Function to be used for curve fitting (default 'linear')
+         Alternatives: 'quadratic','lowess' and 'power_law'
+    xlabel -- Label for the X-axis (default "")
+    ylabel -- Label for the Y-axis (default "")
+    title -- Title for the plot (default "")
     """
     function, xlabel, ylabel, title = kwargs.get('function','linear'), kwargs.get('xlabel',""), kwargs.get('ylabel',""), kwargs.get('title',"")
     fig = plt.figure()
@@ -56,7 +73,23 @@ def scatterplot_fit(X,Y,**kwargs):
     plt.close()
     
 def heatmap_scatterplot(X,Y,Z,**kwargs):
-    """ WRITE DOCUMENTATION.
+    """
+    Takes the X and Y lists and plots them as a scatterplot
+    through matplotlib.with color coding of the points based 
+    on the Z list.
+
+    Keyword arguments:
+    X -- List of the X-coordinates
+    Y -- List of the Y-coordinates
+    Z -- List of the Z-coordinates
+    vmin -- Minimum value to be displayed in the colorbar (default min(Z))
+    vmax -- Maximum value to be displayed in the colorbar (default max(Z))
+    edges -- The edges of each individual datapoint (default 'black')
+    cm -- The colormap used for the colorbar (default 'jet')
+    xlabel -- Label for the X-axis (default "")
+    ylabel -- Label for the Y-axis (default "")
+    zlabel -- Label for the Z-axis (default "")
+    title -- Title for the plot (default "")
     """
     vmin, vmax, edges, cm, xlabel, ylabel, zlabel, title = kwargs.get('vmin',min(Z)), kwargs.get('vmax',max(Z)), kwargs.get('edges','black'), kwargs.get('cm','jet'), kwargs.get('xlabel',""), kwargs.get('ylabel',""), kwargs.get('zlabel',""), kwargs.get('title',"")
     fig = plt.figure()
@@ -76,7 +109,18 @@ def heatmap_scatterplot(X,Y,Z,**kwargs):
     plt.close()
     
 def three_dimension_scatterplot(X,Y,Z,**kwargs):
-    """ WRITE DOCUMENTATION.
+    """
+    Takes the X, Y and Z lists and plots them as a 3D scatter plot
+    through matplotlib.
+    
+    Keyword arguments:
+    X -- List of the X-coordinates
+    Y -- List of the Y-coordinates
+    Z -- List of the Z-coordinates
+    xlabel -- Label for the X-axis (default "")
+    ylabel -- Label for the Y-axis (default "")
+    zlabel -- Label for the Z-axis (default "")
+    title -- Title for the plot (default "")
     """
     xlabel, ylabel, zlabel, title = kwargs.get('xlabel',""), kwargs.get('ylabel',""), kwargs.get('zlabel',""), kwargs.get('title',"")
     fig = plt.figure()
@@ -92,13 +136,50 @@ def three_dimension_scatterplot(X,Y,Z,**kwargs):
     plt.close()
     
 def wireframe(X,Y,Z,**kwargs):
-    """ WRITE DOCUMENTATION
+    """
+    Takes the X, Y and Z lists and plots them as a 3D wireframe
+    through matplotlib.
+    
+    Keyword arguments:
+    X -- List of the X-coordinates
+    Y -- List of the Y-coordinates
+    Z -- List of the Z-coordinates
+    xlabel -- Label for the X-axis (default "")
+    ylabel -- Label for the Y-axis (default "")
+    zlabel -- Label for the Z-axis (default "")
+    title -- Title for the plot (default "")
     """
     xlabel, ylabel, zlabel, title = kwargs.get('xlabel',""), kwargs.get('ylabel',""), kwargs.get('zlabel',""), kwargs.get('title',"")
     fig = plt.figure()
     fig.patch.set_facecolor('white')
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_wireframe(X,Y,Z)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_zlabel(zlabel)
+    ax.set_title(title)
+    plt.show()
+    plt.close()
+    
+def surface(X,Y,Z,**kwargs):
+    """ 
+    Takes the X, Y and Z lists and plots them as a 3D surface plot
+    through matplotlib.
+    
+    Keyword arguments:
+    X -- List of the X-coordinates
+    Y -- List of the Y-coordinates
+    Z -- List of the Z-coordinates
+    xlabel -- Label for the X-axis (default "")
+    ylabel -- Label for the Y-axis (default "")
+    zlabel -- Label for the Z-axis (default "")
+    title -- Title for the plot (default "")
+    """
+    xlabel, ylabel, zlabel, title = kwargs.get('xlabel',""), kwargs.get('ylabel',""), kwargs.get('zlabel',""), kwargs.get('title',"")
+    fig = plt.figure()
+    fig.patch.set_facecolor('white')
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X,Y,Z)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_zlabel(zlabel)
